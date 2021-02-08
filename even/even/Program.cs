c86be53f1d0d5075b12dc49c6664aab8a0c3fa64
NadiaDipa <nadiaislamdipa1@gmail.com>
2021-02-08 09:17:07 +0600

    BasicOperations

---
+++ even/even/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+
+            Console.Write("\n\n");
+            Console.Write("Calculate n terms of even natural number \n");
+            Console.Write("---------------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nThe even numbers are :");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", 2 * i);
+                sum += 2 * i;
+            }
+           
+        }
+    }
+}
+   
+
